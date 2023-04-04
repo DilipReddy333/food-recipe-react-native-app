20f6b1438e44ffb15abe7ad4c683ac42506032d5
DilipReddy333 <87304552+DilipReddy333@users.noreply.github.com>
2023-04-04 13:25:15 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ export const getRecipe = (recipeName) => {
   console.log(recipeName);
   axios
     .get(
-      `https://api.edamam.com/api/recipes/v2?type=public&q=${recipeName}&app_id=8e39c23a&app_key=%2005d61da110b66af90ee209ee131981c4%09`
+      `https://api.edamam.com/api/recipes/v2?type=public&q=${recipeName}&app_id=app_id&app_key=add_key`
     )
     .then((response) => {
       return response.data.hits;
